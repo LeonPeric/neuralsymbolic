55193a6184603b27dad4f8daf9aae1e87da3170d
urbansirca <urban.sirca6@gmail.com>
2025-06-24 14:01:52 +0200

    1000 runs

--- src/pretrain_train.py
+++ src/pretrain_train.py
@@ -8,8 +8,14 @@ from models import LogitsToPredicate, SingleDigitClassifier
 from train import train_logic, train_simple
 
 from utils import plot_training_progression
+import pickle as pkl
 
-for i in range(10):
+metrics_of_runs_logic = []
+metrics_of_runs_pretrain = []
+
+for i in range(2):
+
+    print("======= Run {} =======".format(i))
     if torch.cuda.is_available():
         torch.cuda.manual_seed(i)
         torch.cuda.manual_seed_all(i)
@@ -39,6 +45,9 @@ for i in range(10):
     optimizer = torch.optim.Adam(cnn_s_d.parameters(), lr=0.001)
 
     N_PRETRAIN_EPOCHS = 1
+    N_EPOCHS = 1
+
+    print("Starting pretraining for {} epochs.".format(N_PRETRAIN_EPOCHS))
     pretrain_metrics, pretrained_model = train_simple(
         cnn_s_d,
         optimizer,
@@ -52,8 +61,10 @@ for i in range(10):
 
     optimizer = torch.optim.Adam(pretrained_model.parameters(), lr=0.001)
     Digit_s_d = ltn.Predicate(LogitsToPredicate(pretrained_model)).to(device)
-    
-    print("Pretraining complete. Starting logic training.")
+
+    print(
+        "Pretraining complete. Starting logic training for {} epochs.".format(N_EPOCHS)
+    )
 
     logic_metrics = train_logic(
         Digit_s_d,
@@ -63,12 +74,28 @@ for i in range(10):
         And,
         Exists,
         Forall,
-        n_epochs=1,
+        n_epochs=N_EPOCHS,
         verbose=True,
         save_model=False,
         start_epoch=N_PRETRAIN_EPOCHS,
     )
 
+    metrics_of_runs_pretrain.append(pretrain_metrics)
+    metrics_of_runs_logic.append(logic_metrics)
 
-    plot_training_progression(pretrain_metrics, logic_metrics)
+    # plot_training_progression(pretrain_metrics, logic_metrics)
 
+
+# save metrics_of_runs_pretrain and metrics_of_runs_logic
+
+print("Saving metrics...")
+
+with open("metrics_of_runs_pretrain.pkl", "wb") as f:
+    pkl.dump(metrics_of_runs_pretrain, f)
+
+print("Saved metrics_of_runs_pretrain.pkl")
+
+with open("metrics_of_runs_logic.pkl", "wb") as f:
+    pkl.dump(metrics_of_runs_logic, f)
+
+print("Saved metrics_of_runs_logic.pkl")
